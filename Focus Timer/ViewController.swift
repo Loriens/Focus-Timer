fc8f90fcc7a561d63bc74a1fbdac0cad1a5f72d0
Vladislav Markov <lerd.95@mail.ru>
2019-03-28 00:47:20 +0300

    Add working timer

--- Focus Timer/ViewController.swift
+++ Focus Timer/ViewController.swift
@@ -14,23 +14,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
     
     private var buttonState = ButtonState.stop
+    private var timer: Timer!
+    private var timerSeconds = 25 * 60
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(TimeParser.stringTime(from: 600))
-        print(TimeParser.stringTime(from: 599))
-        print(TimeParser.stringTime(from: 543))
+        timeLabel.text = TimeParser.stringTime(from: timerSeconds)
     }
 
     @IBAction func startStopButtonPressed(_ sender: Any) {
         if buttonState == .stop {
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerUpdate(_:)), userInfo: nil, repeats: true)
             startStopButton.setImage(UIImage(named: "stop"), for: .normal)
             buttonState = .play
         } else {
-            startStopButton.setImage(UIImage(named: "play"), for: .normal)
             buttonState = .stop
+            startStopButton.setImage(UIImage(named: "play"), for: .normal)
         }
+    }
+    
+    @objc func timerUpdate(_ timer: Timer) {
+        if buttonState == .stop {
+            timer.invalidate()
+        }
+        
+        timerSeconds = timerSeconds - 1
+        timeLabel.text = TimeParser.stringTime(from: timerSeconds)
     }
     
 }
