be7bb5f2ee262cb0fe704669dbfc124f75d9cec4
Vladislav Markov <lerd.95@mail.ru>
2019-03-28 01:09:08 +0300

    Fix

--- Focus Timer/ViewController.swift
+++ Focus Timer/ViewController.swift
@@ -15,15 +15,35 @@ class ViewController: UIViewController {
     
     private var buttonState = ButtonState.stop
     private var timer: Timer!
-    private var timerSeconds = 25 * 60
+    private var mainTimerSeconds = 10
+    private var timerSeconds: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        timeLabel.text = TimeParser.stringTime(from: timerSeconds)
+        setMainTimerSeconds()
     }
 
     @IBAction func startStopButtonPressed(_ sender: Any) {
+        toggleButtonStartStop()
+    }
+    
+    @objc func timerUpdate(_ timer: Timer) {
+        if buttonState == .stop {
+            timer.invalidate()
+            return
+        } else if timerSeconds == 1 {
+            timer.invalidate()
+            toggleButtonStartStop()
+            setMainTimerSeconds()
+            return
+        }
+        
+        timerSeconds = timerSeconds - 1
+        timeLabel.text = TimeParser.stringTime(from: timerSeconds)
+    }
+    
+    private func toggleButtonStartStop() {
         if buttonState == .stop {
             timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerUpdate(_:)), userInfo: nil, repeats: true)
             startStopButton.setImage(UIImage(named: "stop"), for: .normal)
@@ -34,12 +54,8 @@ class ViewController: UIViewController {
         }
     }
     
-    @objc func timerUpdate(_ timer: Timer) {
-        if buttonState == .stop {
-            timer.invalidate()
-        }
-        
-        timerSeconds = timerSeconds - 1
+    private func setMainTimerSeconds() {
+        timerSeconds = mainTimerSeconds
         timeLabel.text = TimeParser.stringTime(from: timerSeconds)
     }
     
