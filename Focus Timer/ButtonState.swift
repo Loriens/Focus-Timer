833ca997aeb07a9103b7d69ec072fd116b4fecd5
Vladislav Markov <lerd.95@mail.ru>
2019-03-27 21:42:43 +0300

    Add action to startStopButton

---
+++ Focus Timer/ButtonState.swift
@@ -0,0 +1,15 @@
+//
+//  ButtonState.swift
+//  Focus Timer
+//
+//  Created by Vladislav on 27/03/2019.
+//  Copyright © 2019 Vladislav Markov. All rights reserved.
+//
+
+import Foundation
+
+
+enum ButtonState {
+    case start
+    case stop
+}
--- Focus Timer/ViewController.swift
+++ Focus Timer/ViewController.swift
@@ -10,14 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var startPauseButton: UIButton!
+    @IBOutlet weak var startStopButton: UIButton!
+    private var buttonState = ButtonState.stop
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    @IBAction func pressedStartPauseButton(_ sender: Any) {
-        
+    @IBAction func startStopButtonPressed(_ sender: Any) {
+        if buttonState == .stop {
+            startStopButton.setImage(UIImage(named: "play"), for: .normal)
+        } else {
+            startStopButton.setImage(UIImage(named: "stop"), for: .normal)
+        }
     }
     
 }
