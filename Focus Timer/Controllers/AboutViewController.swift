b77fd4e9403fb33fbbeff27934b8e5421876c963
Vladislav Markov <lerd.95@mail.ru>
2019-03-29 01:53:19 +0300

    Add AboutViewController

---
+++ Focus Timer/Controllers/AboutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AboutViewController.swift
+//  Focus Timer
+//
+//  Created by Vladislav on 29/03/2019.
+//  Copyright © 2019 Vladislav Markov. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "О приложении"
+    }
+
+}
