0e7b7c1664f9d5b5de9dfaabb8e1a06897d3fde4
Vladislav Markov <lerd.95@mail.ru>
2019-03-28 23:51:51 +0300

    Add enum Key for UserDefaults

---
+++ Focus Timer/Key.swift
@@ -0,0 +1,14 @@
+//
+//  Key.swift
+//  Focus Timer
+//
+//  Created by Vladislav on 28/03/2019.
+//  Copyright © 2019 Vladislav Markov. All rights reserved.
+//
+
+import Foundation
+
+enum Key {
+    case workingTime
+    case breakingTime
+}
