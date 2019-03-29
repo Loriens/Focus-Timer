534b7b7f5d13139e4ed031ee092911448278b60c
Vladislav Markov <lerd.95@mail.ru>
2019-03-29 17:18:50 +0300

    Add global constants in Const

---
+++ Focus Timer/Model/Const.swift
@@ -0,0 +1,15 @@
+//
+//  Const.swift
+//  Focus Timer
+//
+//  Created by Vladislav on 29/03/2019.
+//  Copyright © 2019 Vladislav Markov. All rights reserved.
+//
+
+import Foundation
+
+class Const {
+    
+    static let sounds: [String: Int] = ["Bloom": 1021, "Calypso": 1022, "Choo Choo": 1023, "Descent": 1024, "Fanfare": 1025, "Ladder": 1026, "News Flash": 1028, "Noir": 1029, "Update": 1036]
+    
+}
